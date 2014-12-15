e41dba2c45461a9f18ecfdb8acad1e0ffcda2e00
Vicent Alabau <valabau@prhlt.upv.es>
2014-12-15 16:08:52 +0100

    Update git repository in install.sh script

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 
 apt-get -yq update
 apt-get -yq install git
-git clone https://github.com/phikoehn/casmacat-install.git /opt/casmacat
+git clone https://github.com/casmacat/casmacat-home-edition.git /opt/casmacat
 cd /opt/casmacat/install
 sh install.sh
