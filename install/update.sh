62070dce9d32b979d3b0ef53f387b7b61abc19a4
Philipp Koehn <phi@jhu.edu>
2014-12-19 15:43:48 -0500

    also update thot

--- install/update.sh
+++ install/update.sh
@@ -10,3 +10,5 @@ mkdir $LOGDIR
 
 sh ./install-moses.sh > $LOGDIR/moses.out 2> $LOGDIR/moses.err &
 sh ./install-casmacat.sh > $LOGDIR/casmacat.out 2> $LOGDIR/casmacat.err &
+sh ./install-thot.sh > $LOGDIR/thot.out 2> $LOGDIR/thor.err &
+sh ./install-casmacat-upvlc.sh > $LOGDIR/casmacat-upvlc.out 2> $LOGDIR/casmacat-upvlc.err &
