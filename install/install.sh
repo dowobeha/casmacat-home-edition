cb3eb8ed94a9b46f631fed615889802fa8103030
Lane Schwartz <dowobeha@gmail.com>
2016-10-17 13:55:11 -0500

    Use bash instead of sh

--- install/install.sh
+++ install/install.sh
@@ -11,18 +11,18 @@ cd /opt/casmacat/install
 mkdir -p $LOGDIR
 
 
-sh ./install-dependencies.sh > $LOGDIR/dependencies.out 2> $LOGDIR/dependencies.err
+bash ./install-dependencies.sh > $LOGDIR/dependencies.out 2> $LOGDIR/dependencies.err
 chown -R $user:$user /opt/casmacat
 bash ./setup-admin.sh > $LOGDIR/admin.out 2> $LOGDIR/admin.err &
-sh ./install-dependencies2.sh >> $LOGDIR/dependencies.out 2>> $LOGDIR/dependencies.err
+bash ./install-dependencies2.sh >> $LOGDIR/dependencies.out 2>> $LOGDIR/dependencies.err
 
 if test "$INSTALL_MOSES" = "yes"; then 
-  sh ./install-moses.sh > $LOGDIR/moses.out 2> $LOGDIR/moses.err &
-  sh ./install-casmacat.sh > $LOGDIR/casmacat.out 2> $LOGDIR/casmacat.err &
-  sh ./download-test-model.sh > $LOGDIR/test-model.out 2> $LOGDIR/test-model.err &
+  bash ./install-moses.sh > $LOGDIR/moses.out 2> $LOGDIR/moses.err &
+  bash ./install-casmacat.sh > $LOGDIR/casmacat.out 2> $LOGDIR/casmacat.err &
+  bash ./download-test-model.sh > $LOGDIR/test-model.out 2> $LOGDIR/test-model.err &
 fi
 
 if test "$INSTALL_THOT" = "yes"; then 
-  sh ./install-casmacat-upvlc.sh > $LOGDIR/casmacat-upvlc.out 2> $LOGDIR/casmacat-upvlc.err 
-  sh ./install-thot.sh > $LOGDIR/thot.out 2> $LOGDIR/thot.err 
+  bash ./install-casmacat-upvlc.sh > $LOGDIR/casmacat-upvlc.out 2> $LOGDIR/casmacat-upvlc.err 
+  bash ./install-thot.sh > $LOGDIR/thot.out 2> $LOGDIR/thot.err 
 fi
